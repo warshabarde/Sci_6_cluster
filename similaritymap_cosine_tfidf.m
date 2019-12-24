5201773f598f16336bbdf00e43e52e888c574cba
warsha <warsha@oleic.co>
2019-12-24 18:00:18 +0530

    initial codes added

---
+++ similaritymap_cosine_tfidf.m
@@ -0,0 +1,17 @@
+clear all
+close all
+
+load('Cosine_sim_index_tfidf_6.mat')
+
+%calculate distance
+Cosine_dis=1-cosine_sim_index_tfidf;
+
+%make all diagonal elements 0
+index = find(tril(ones(size(Cosine_dis)), -1));
+C = Cosine_dis(index);
+test=squareform(C);
+
+% make a vector to feed into linkage fn
+y = squareform(test);
+Z = linkage(y,'complete');
+dendrogram(Z,0)
